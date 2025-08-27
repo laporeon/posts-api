803990e86bb267266809439ee056f31d43626c43
laporeon <175569928+laporeon@users.noreply.github.com>
2025-08-27 15:21:29 -0300

    feat: create post DTO

---
+++ src/main/java/com/laporeon/posts_api/dto/PostDTO.java
@@ -0,0 +1,16 @@
+package com.laporeon.posts_api.dto;
+
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record PostDTO(
+        @NotBlank(message = "Title is required.")
+        @Size(min = 3, max = 100, message = "Title must be between {min} and {max} characters long.")
+        String title,
+        @NotBlank(message = "Description is required.")
+        @Size(min = 10, max = 250, message = "Description must be between {min} and {max} characters long.")
+        String description,
+        @NotBlank(message = "Body content is required.")
+        @Size(min = 10, max = 500, message = "Body content must be between {min} and {max} characters long.")
+        String body
+) {}
