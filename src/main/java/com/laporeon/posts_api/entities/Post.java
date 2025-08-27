d370831c7b4819f5b7e1fc6e0b8763e8fd338667
laporeon <175569928+laporeon@users.noreply.github.com>
2025-08-27 15:21:52 -0300

    feat: create post entity

---
+++ src/main/java/com/laporeon/posts_api/entities/Post.java
@@ -0,0 +1,38 @@
+package com.laporeon.posts_api.entities;
+
+import com.laporeon.posts_api.dto.PostDTO;
+import lombok.Data;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.MongoId;
+
+import java.time.LocalDateTime;
+
+@Document(collection = "posts")
+@Data
+public class Post {
+
+    @MongoId
+    public String id;
+
+    public String title;
+
+    public String description;
+
+    public String body;
+
+    @CreatedDate
+    public LocalDateTime createdAt;
+
+    @LastModifiedDate
+    public LocalDateTime updatedAt;
+
+    public static Post fromDTO(PostDTO postDTO) {
+        Post post = new Post();
+        post.setTitle(postDTO.title());
+        post.setDescription(postDTO.description());
+        post.setBody(postDTO.body());
+        return post;
+    }
+}
