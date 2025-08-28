cb232d50baf291c35535e51107836d756d8f0cf8
laporeon <175569928+laporeon@users.noreply.github.com>
2025-08-28 14:20:21 -0300

    feat: create getAllPosts method

--- src/main/java/com/laporeon/posts_api/controllers/PostController.java
+++ src/main/java/com/laporeon/posts_api/controllers/PostController.java
@@ -8,12 +8,25 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.List;
+
 @RestController
 @RequestMapping("/posts")
 @RequiredArgsConstructor
 public class PostController {
 
     private final PostService postService;
+
+    @GetMapping
+    public ResponseEntity<List<Post>> getAllPosts() {
+        List<Post> posts = postService.getAllPosts();
+
+        if (posts.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+
+        return ResponseEntity.ok().body(posts);
+    }
 
     @PostMapping
     public ResponseEntity<Post> createPost(@Valid @RequestBody PostDTO postDTO) {
