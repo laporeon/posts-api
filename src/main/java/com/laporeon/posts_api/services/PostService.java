fd890245e8fb838c0e4b5c942b841c29c6df7d70
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-02 16:02:29 -0300

    feat: create findPostById and deletePost methods on service layer

--- src/main/java/com/laporeon/posts_api/services/PostService.java
+++ src/main/java/com/laporeon/posts_api/services/PostService.java
@@ -2,6 +2,7 @@ package com.laporeon.posts_api.services;
 
 import com.laporeon.posts_api.dto.PostDTO;
 import com.laporeon.posts_api.entities.Post;
+import com.laporeon.posts_api.exceptions.PostNotFoundException;
 import com.laporeon.posts_api.repositories.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -19,8 +20,24 @@ public class PostService {
         return postRepository.save(post);
     }
 
+    public Post findById(String id) {
+        return postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("Post not found."));
+    }
+
     public List<Post> getAllPosts() {
-        return postRepository.findAll();
+        List<Post> postList = postRepository.findAll();
+
+        if(postList.isEmpty()) {
+            throw new PostNotFoundException("No posts found.");
+        }
+
+        return postList;
+    }
+
+    public void deletePost(String id) {
+        postRepository.findById(id).orElseThrow(() -> new PostNotFoundException("Post not found."));
+
+        postRepository.deleteById(id);
     }
 
 }
