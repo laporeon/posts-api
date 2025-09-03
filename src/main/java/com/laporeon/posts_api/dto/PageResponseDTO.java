5c39f547daf1b4f063f909362fcd5a8e22be24af
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-03 16:01:54 -0300

    feat: create DTO for pageable response

---
+++ src/main/java/com/laporeon/posts_api/dto/PageResponseDTO.java
@@ -0,0 +1,16 @@
+package com.laporeon.posts_api.dto;
+
+import java.util.List;
+
+public record PageResponseDTO<T>(List<T> content,
+                              int pageNumber,
+                              int pageSize,
+                              int totalPages,
+                              long totalElements,
+                              int numberOfElements,
+                              boolean isFirstPage,
+                              boolean isLastPage,
+                              boolean isEmpty,
+                              boolean isSorted,
+                              boolean isUnsorted) {
+}
