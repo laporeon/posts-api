2cbd6c65b283de78014fede94593606c8f5fd14e
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-03 14:32:41 -0300

    refactor: create constants for exception messages and new constructor

--- src/main/java/com/laporeon/posts_api/exceptions/PostNotFoundException.java
+++ src/main/java/com/laporeon/posts_api/exceptions/PostNotFoundException.java
@@ -1,8 +1,15 @@
 package com.laporeon.posts_api.exceptions;
 
 public class PostNotFoundException extends RuntimeException {
+    private static final String DEFAULT_MESSAGE = "No posts found.";
+    private static final String MESSAGE_WITH_ID = "Post with id %s not found.";
 
-    public PostNotFoundException(String message) {
-        super(message);
+    public PostNotFoundException() {
+        super(DEFAULT_MESSAGE);
     }
+
+    public PostNotFoundException(String id) {
+        super(MESSAGE_WITH_ID.formatted(id));
+    }
+
 }
