cbf724f6e70d02845e66b1512e2c1261f419a2b7
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-03 16:01:31 -0300

    refactor: create default message constant for PostNotFoundException

--- src/main/java/com/laporeon/posts_api/exceptions/PostNotFoundException.java
+++ src/main/java/com/laporeon/posts_api/exceptions/PostNotFoundException.java
@@ -1,15 +1,10 @@
 package com.laporeon.posts_api.exceptions;
 
 public class PostNotFoundException extends RuntimeException {
-    private static final String DEFAULT_MESSAGE = "No posts found.";
-    private static final String MESSAGE_WITH_ID = "Post with id %s not found.";
-
-    public PostNotFoundException() {
-        super(DEFAULT_MESSAGE);
-    }
+    private static final String DEFAULT_MESSAGE = "Post with id %s not found.";
 
     public PostNotFoundException(String id) {
-        super(MESSAGE_WITH_ID.formatted(id));
+        super(DEFAULT_MESSAGE.formatted(id));
     }
 
 }
