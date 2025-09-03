2c76eb4e66e3387befe97780d7376d30be44027f
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-03 14:32:13 -0300

    refactor: add @Field annotation at createdAt and updatedAt

--- src/main/java/com/laporeon/posts_api/entities/Post.java
+++ src/main/java/com/laporeon/posts_api/entities/Post.java
@@ -5,6 +5,7 @@ import lombok.Data;
 import org.springframework.data.annotation.CreatedDate;
 import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.mongodb.core.mapping.Document;
+import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
 import java.time.LocalDateTime;
@@ -23,9 +24,11 @@ public class Post {
     public String body;
 
     @CreatedDate
+    @Field("created_at")
     public LocalDateTime createdAt;
 
     @LastModifiedDate
+    @Field("updated_at")
     public LocalDateTime updatedAt;
 
     public static Post fromDTO(PostDTO postDTO) {
