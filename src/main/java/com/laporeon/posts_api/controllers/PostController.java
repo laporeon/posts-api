c3c98ae31015c25fbde62f213ee44ce2c101f831
laporeon <175569928+laporeon@users.noreply.github.com>
2025-09-03 16:00:17 -0300

    feat: create pagination for get all posts method on controller layer

--- src/main/java/com/laporeon/posts_api/controllers/PostController.java
+++ src/main/java/com/laporeon/posts_api/controllers/PostController.java
@@ -1,17 +1,20 @@
 package com.laporeon.posts_api.controllers;
 
+import com.laporeon.posts_api.dto.PageResponseDTO;
 import com.laporeon.posts_api.dto.PostDTO;
 import com.laporeon.posts_api.entities.Post;
 import com.laporeon.posts_api.services.PostService;
+import com.laporeon.posts_api.mappers.PostPageMapper;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import java.net.URI;
-import java.util.List;
 
 @RestController
 @RequestMapping("/posts")
@@ -19,13 +22,23 @@ import java.util.List;
 public class PostController {
 
     private final PostService postService;
+    private final PostPageMapper mapper;
 
-    // TODO: Create pagination for getAllPosts method
     @GetMapping
-    public ResponseEntity<List<Post>> getAllPosts() {
-        List<Post> posts = postService.getAllPosts();
-        return ResponseEntity.ok().body(posts);
+    public ResponseEntity<PageResponseDTO> getAllPosts(
+            @RequestParam(value = "page", required = false, defaultValue = "0") int page,
+            @RequestParam(value = "size", required = false, defaultValue = "5") int size,
+            @RequestParam(value = "orderBy", required = false, defaultValue = "title") String orderBy,
+            @RequestParam(value = "direction", defaultValue = "ASC") String direction) {
+
+        Pageable pageable = PageRequest.of(page, size,
+                Sort.by(Sort.Direction.valueOf(direction.toUpperCase()), orderBy));
+
+        Page<Post> posts = postService.getAllPosts(pageable);
+        PageResponseDTO<Post> response = mapper.toDto(posts);
+        return ResponseEntity.ok().body(response);
     }
+
 
     @GetMapping("/{id}")
     public ResponseEntity<Post> findPostById(@PathVariable("id") String id) {
