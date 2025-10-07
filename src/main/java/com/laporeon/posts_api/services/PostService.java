4a8cd12b3ecdb6f7c646a47e16b3b9a13662e42e
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-07 15:57:46 -0300

    refactor: implement mappers on service layer and change return type to match result expected by controller layer

--- src/main/java/com/laporeon/posts_api/services/PostService.java
+++ src/main/java/com/laporeon/posts_api/services/PostService.java
@@ -1,9 +1,12 @@
 package com.laporeon.posts_api.services;
 
 import com.laporeon.posts_api.dto.request.PostRequestDTO;
+import com.laporeon.posts_api.dto.response.PageResponseDTO;
+import com.laporeon.posts_api.dto.response.PostResponseDTO;
 import com.laporeon.posts_api.entities.Post;
 import com.laporeon.posts_api.exceptions.PostNotFoundException;
 import com.laporeon.posts_api.mappers.PostMapper;
+import com.laporeon.posts_api.mappers.PageMapper;
 import com.laporeon.posts_api.repositories.PostRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
@@ -15,32 +18,36 @@ import org.springframework.stereotype.Service;
 public class PostService {
 
     private final PostRepository postRepository;
+
     private final PostMapper postMapper;
 
-    public Post create(PostRequestDTO postRequestDTO) {
-        Post post = postMapper.toEntity(postRequestDTO);
-        return postRepository.save(post);
+    private final PageMapper paginationMapper;
+
+    public PostResponseDTO create(PostRequestDTO dto) {
+        Post post = postRepository.save(postMapper.toEntity(dto));
+        return postMapper.toDTO(post);
     }
 
-    public Post findById(String id) {
-        return postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
+    public PageResponseDTO<PostResponseDTO> listPosts(Pageable pageable) {
+        Page<Post> posts = postRepository.findAll(pageable);
+        PageResponseDTO<PostResponseDTO> pageResponseDTO = paginationMapper.toDTO(posts);
+        return pageResponseDTO;
     }
 
-    public Page<Post> getAllPosts(Pageable pageable) {
-        return postRepository.findAll(pageable);
+    public PostResponseDTO findById(String id) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
+        return postMapper.toDTO(post);
     }
 
-    public Post updatePost(String id, PostRequestDTO postRequestDTO) {
-        Post existingPost = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
-        
-        existingPost.setTitle(postRequestDTO.title());
-        existingPost.setDescription(postRequestDTO.description());
-        existingPost.setBody(postRequestDTO.body());
+    public PostResponseDTO update(String id, PostRequestDTO dto) {
+        Post post = postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
 
-        return postRepository.save(existingPost);
+        post = postRepository.save(postMapper.updateEntityFromDTO(dto, post));
+
+        return postMapper.toDTO(post);
     }
 
-    public void deletePost(String id) {
+    public void delete(String id) {
         postRepository.findById(id).orElseThrow(() -> new PostNotFoundException(id));
 
         postRepository.deleteById(id);
