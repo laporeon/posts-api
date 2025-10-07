5f51026aa31b948e8a1208104329cc97ef597e06
laporeon <175569928+laporeon@users.noreply.github.com>
2025-10-07 15:55:17 -0300

    refactor: rename PageResponseDTO to improve naming consistency

--- src/main/java/com/laporeon/posts_api/dto/response/PageResponseDTO.java
+++ src/main/java/com/laporeon/posts_api/dto/response/PageResponseDTO.java
@@ -2,15 +2,15 @@ package com.laporeon.posts_api.dto.response;
 
 import java.util.List;
 
-public record PageablePostResponseDTO<T>(List<T> content,
-                                         int pageNumber,
-                                         int pageSize,
-                                         int totalPages,
-                                         long totalElements,
-                                         int numberOfElements,
-                                         boolean isFirstPage,
-                                         boolean isLastPage,
-                                         boolean isEmpty,
-                                         boolean isSorted,
-                                         boolean isUnsorted) {
+public record PageResponseDTO<T>(List<T> content,
+                                 int pageNumber,
+                                 int pageSize,
+                                 int totalPages,
+                                 long totalElements,
+                                 int numberOfElements,
+                                 boolean isFirstPage,
+                                 boolean isLastPage,
+                                 boolean isEmpty,
+                                 boolean isSorted,
+                                 boolean isUnsorted) {
 }
