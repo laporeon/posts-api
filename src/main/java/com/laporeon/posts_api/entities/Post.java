028a8651257e84b91c81548a02d964810c86db53
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-04 16:33:02 -0300

    refactor(entities): replace LocalDateTime with Instant and add Builder annotation in Post Entity

--- src/main/java/com/laporeon/posts_api/entities/Post.java
+++ src/main/java/com/laporeon/posts_api/entities/Post.java
@@ -1,6 +1,7 @@
 package com.laporeon.posts_api.entities;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.springframework.data.annotation.CreatedDate;
@@ -9,10 +10,11 @@ import org.springframework.data.mongodb.core.mapping.Document;
 import org.springframework.data.mongodb.core.mapping.Field;
 import org.springframework.data.mongodb.core.mapping.MongoId;
 
-import java.time.LocalDateTime;
+import java.time.Instant;
 
 @Document(collection = "posts")
 @Data
+@Builder
 @AllArgsConstructor
 @NoArgsConstructor
 public class Post {
@@ -28,10 +30,10 @@ public class Post {
 
     @CreatedDate
     @Field("created_at")
-    private LocalDateTime createdAt;
+    private Instant createdAt;
 
     @LastModifiedDate
     @Field("updated_at")
-    private LocalDateTime updatedAt;
+    private Instant updatedAt;
 
 }
