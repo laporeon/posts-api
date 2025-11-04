f84fbeb6b4cffe8f5fa11ebe5a50b61e7430e5f0
laporeon <175569928+laporeon@users.noreply.github.com>
2025-11-04 16:30:39 -0300

    refactor(dtos): update min and max values for request attributes

--- src/main/java/com/laporeon/posts_api/dto/request/PostRequestDTO.java
+++ src/main/java/com/laporeon/posts_api/dto/request/PostRequestDTO.java
@@ -5,12 +5,12 @@ import jakarta.validation.constraints.Size;
 
 public record PostRequestDTO(
         @NotBlank(message = "Title is required.")
-        @Size(min = 3, max = 100, message = "Title must be between {min} and {max} characters long.")
+        @Size(min = 10, max = 100, message = "Title must be between {min} and {max} characters long.")
         String title,
         @NotBlank(message = "Description is required.")
-        @Size(min = 10, max = 250, message = "Description must be between {min} and {max} characters long.")
+        @Size(min = 20, max = 150, message = "Description must be between {min} and {max} characters long.")
         String description,
         @NotBlank(message = "Body content is required.")
-        @Size(min = 10, max = 500, message = "Body content must be between {min} and {max} characters long.")
+        @Size(min = 60, max = 500, message = "Body content must be between {min} and {max} characters long.")
         String body
 ) {}
